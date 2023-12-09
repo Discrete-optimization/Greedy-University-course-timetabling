06366c9bdc36b1ac501a4d17067308151d2f663a
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-09 20:11:02 +0330

    printer

--- queue.py
+++ queue.py
@@ -4,8 +4,11 @@ class Queue:
 
     def get_list(self):
         return self.list
+    def printer(self):
+        for element in self.list:
+            print("{} with value: {}".format(element[0], element[1]))
 
 
 list = [('A', 3), ('B', 3), ('C', 3), ('D', 2), ('E', 2), ('F', 1)]
 Q = Queue(list)
-print(Q.get_list())
+Q.printer()
