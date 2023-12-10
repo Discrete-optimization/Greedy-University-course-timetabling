86072ef94bead23714ef6bc91ffa82eedd33768f
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-10 18:32:30 +0330

    insertionSort

--- queue.py
+++ queue.py
@@ -2,6 +2,20 @@ class Sorting:
     def __init__(self, array):
         self.array = array
 
+    def insertionSort(self):
+        n = len(self.array)  # Get the length of the array
+
+        if n <= 1:
+            return  # If the array has 0 or 1 element, it is already sorted, so return
+
+        for i in range(1, n):  # Iterate over the array starting from the second element
+            key = self.array[i][1]  # Store the current element as the key to be inserted in the right position
+            j = i - 1
+            while j >= 0 and key < self.array[j][1]:  # Move elements greater than key one position ahead
+                self.array[j + 1][1] = self.array[j][1]  # Shift elements to the right
+                j -= 1
+            self.array[j + 1][1] = key  # Insert the key in the correct position
+
 class Queue:
     def __init__(self, list):
         self.list = list
