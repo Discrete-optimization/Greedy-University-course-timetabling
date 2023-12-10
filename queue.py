3652c65caeb918391c0eba61f9845a2319cdd257
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-10 18:38:46 +0330

    test insertionSort

--- queue.py
+++ queue.py
@@ -6,18 +6,7 @@ class Sorting:
         return self.array
 
     def insertionSort(self):
-        n = len(self.array)  # Get the length of the array
-
-        if n <= 1:
-            return  # If the array has 0 or 1 element, it is already sorted, so return
-
-        for i in range(1, n):  # Iterate over the array starting from the second element
-            key = self.array[i][1]  # Store the current element as the key to be inserted in the right position
-            j = i - 1
-            while j >= 0 and key < self.array[j][1]:  # Move elements greater than key one position ahead
-                self.array[j + 1][1] = self.array[j][1]  # Shift elements to the right
-                j -= 1
-            self.array[j + 1][1] = key  # Insert the key in the correct position
+        self.array.sort(key=lambda x: x[1])
 
 class Queue:
     def __init__(self, list):
@@ -49,6 +38,6 @@ class Queue:
 
 
 list = [('A', 3), ('B', 3), ('C', 3), ('D', 2), ('E', 2), ('F', 1)]
-S = Sorting
+S = Sorting(list)
 S.insertionSort()
 print(S.get_array())
