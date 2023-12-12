f4788b53fed8c87c6b27c5076f5642ae87d0a7f0
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-12 13:23:19 +0330

    new Data

--- data.py
+++ data.py
@@ -4,17 +4,17 @@ class Data:
         self.classRooms = ['ClassRoom 1', 'ClassRoom 2', 'ClassRoom 3']
         self.classTimes = ['08:00 - 10:00', '10:00 - 12:00', '12:00 - 14:00']
         self.cources = [
-            ['cource 1', 3]
-            ['cource 2', 3]
-            ['cource 3', 3]
-            ['cource 4', 3]
-            ['cource 5', 3]
-            ['cource 6', 3]
-            ['cource 7', 3]
-            ['cource 8', 2]
-            ['cource 9', 2]
-            ['cource 10', 1]
-            ['cource 11', 1]
+            ('cource 1', 3)
+            ('cource 2', 3)
+            ('cource 3', 3)
+            ('cource 4', 3)
+            ('cource 5', 3)
+            ('cource 6', 3)
+            ('cource 7', 3)
+            ('cource 8', 2)
+            ('cource 9', 2)
+            ('cource 10', 1)
+            ('cource 11', 1)
         ]
 
 
